43924af10bd1f69aefe0070443754f7c3d90f124
Younis <ya945055@gmail.com>
2026-03-02 09:26:51 +0500

    Refactor user routes: add account update, avatar and cover image upload, and user channel profile and watch history retrieval endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshToken, registeruser } from "../controllers/user.controller.js";
+import { getAccountDetails, getCurrentUser, getuserChannelProfile, getUserWatchHistory, loginUser, logoutUser, refreshToken, registeruser, updateAvatar, updateCoverImage, updatePassword } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -24,5 +24,19 @@ router.route('/login').post(loginUser);
 router.route('/logout').post(verifyJWT , logoutUser);
 
 router.route('/refresh-token').post(refreshToken);
+
+router.route('/update-password').post(verifyJWT , updatePassword);
+
+router.route('/current-user', verifyJWT , getCurrentUser);
+
+router.route('/update-account').patch(getAccountDetails);
+
+router.route('/update-avatar').patch(verifyJWT , upload.single('avatar') , updateAvatar);
+
+router.route('/update-coverImage').patch(verifyJWT , upload.single('coverImage') , updateCoverImage);
+
+router.route('/channel/:username').get(verifyJWT ,  getuserChannelProfile);
+
+router.route('/watch-History').get(verifyJWT , getUserWatchHistory);
 
 export default router;
